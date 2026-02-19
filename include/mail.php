7386c46860eb4ec4f35381e73a54bf024190f76e
PREM SUDESH BARASKAR <bubbbdoo@gmail.com>
2026-02-19 17:55:00 +0530

    Remove email credentials from mail.php
    Removed hardcoded email credentials for security.

--- include/mail.php
+++ include/mail.php
@@ -11,12 +11,12 @@ function sendMail($to, $subject, $body) {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'prem.baraskar24@sakec.ac.in';
-    $mail->Password = 'ngkg qoep tgel hoys';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
-    $mail->setFrom('prem.baraskar24@sakec.ac.in', 'Global Hospitals');
+    $mail->setFrom('', 'Global Hospitals');
     $mail->addAddress($to);
 
     $mail->isHTML(true);
